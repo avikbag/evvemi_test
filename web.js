ac17dbf3e64865d9fe0193c35f7d41af0eacd9f2
Avik Bag <avikbag@gmail.com>
2017-05-26 16:23:08 -0400

    Added database connections for nodejs

--- web.js
+++ web.js
@@ -6,3 +6,49 @@ var app = express();
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
+
+// NodeJS Connection to Postgres
+
+var Pool = require('pg').Pool;
+var pool = new Pool({
+  user: RDS_user,
+  password: RDS_pass,
+  host: RDS_url,
+  database: 'leverjs',
+  //max: 10, // max number of clients in pool
+  //idleTimeoutMillis: 1000, // close & remove clients which have been idle > 1 second
+});
+
+pool.on('error', function(e, client) {
+  // if a client is idle in the pool
+  // and receives an error - for example when your PostgreSQL server restarts
+  // the pool will catch the error & let you handle it here
+	console.log('There was some sort of error')
+});
+
+var ct = `CREATE Table Student{ 
+            name varchar(128),
+            id integer;
+}`;
+
+// you can run queries directly against the pool
+pool.query(ct, function(err, result) {
+  console.log(result); // output: foo
+});
+
+//// the query object implements the promise API
+//pool.query('SELECT $1::text as name', ['foo'])
+  //.then(res => console.log(res.rows[0].name)); // output: foo
+
+//// the pool also supports checking out a client for
+//// multiple operations, such as a transaction
+
+//pool.connect(function(err, client, release) {
+  //// TODO - you'll want to handle the error in real code
+
+  //client.query('SELECT $1::text as name', ['foo'], function(err, result) {
+    //// you MUST return your client back to the pool when you're done!
+    //release();
+    //console.log(result.rows[0].name); // output: foo
+  //});
+//});
