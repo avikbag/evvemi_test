6dc85b13f33fd5c08c5d97ae29aa061345a06278
Avik Bag <avikbag@gmail.com>
2017-05-26 11:52:13 -0400

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evvemiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the evvemiApp
+ */
+angular.module('evvemiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evvemiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the evvemiApp
+ */
+angular.module('evvemiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
