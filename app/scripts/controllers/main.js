b7e220c6b43473915b57b159a86739eea11c312d
Avik Bag <avikbag@gmail.com>
2017-05-27 16:34:36 -0400

    changed to success

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,8 +16,8 @@ angular.module('evvemiApp')
     ];
   $scope.query = function(){
     console.log('Catch');
-		var data = {test: 'This is a test run'};
-		$http.get("/api/query/", data).then(function(output) {
+		var data = {"test": 'This is a test run'};
+		$http.get("/api/query/", data).success(function(output) {
         console.log('Data posted successfully');
         console.log(output);
     });
