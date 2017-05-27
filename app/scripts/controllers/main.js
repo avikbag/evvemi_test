062dc0e8ace3c4fe14825e066be797e7ca8f7390
Avik Bag <avikbag@gmail.com>
2017-05-27 16:36:31 -0400

    reevert to then

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,7 +17,7 @@ angular.module('evvemiApp')
   $scope.query = function(){
     console.log('Catch');
 		var data = {"test": 'This is a test run'};
-		$http.get("/api/query/", data).success(function(output) {
+		$http.get("/api/query/", data).then(function(output) {
         console.log('Data posted successfully');
         console.log(output);
     });
