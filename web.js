4025b11acf3aa9f00564947368106ffd303c37c5
Avik Bag <avikbag@gmail.com>
2017-05-27 17:15:37 -0400

    req body test

--- web.js
+++ web.js
@@ -37,7 +37,7 @@ var ct = `CREATE Table Potato(
 
 // you can run queries directly against the pool
 app.post('/api/query', function(res, req){
-  console.log(res);
+  console.log(res.body);
   req.write('Data');
   req.end();
   //pool.query(ct, function(err, result) {
