1d9228314818d0789d13f4a3d94061ab5a063bc9
Avik Bag <avikbag@gmail.com>
2017-05-27 16:49:17 -0400

    added paramterized data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,7 +16,7 @@ angular.module('evvemiApp')
     ];
   $scope.query = function(){
     console.log('Catch');
-		var data = {"test": 'This is a test run'};
+		var data = $.param({"test": 'This is a test run'});
 		$http.get("/api/query/", data).then(function(output) {
         console.log('Data posted successfully');
         console.log(output);
