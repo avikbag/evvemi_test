e1d9b3bb34e18f173ab0291f59a51484ca094e12
Avik Bag <avikbag@gmail.com>
2017-05-27 20:06:02 -0400

    fixed minor mistakes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,14 @@
  */
 angular.module('evvemiApp')
   .controller('MainCtrl', function ($scope, $http) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.queryData = {
+      name_id: null,
+      name: ''
+    };
+    
     var str_test = "select * from student";
-    var insertStudent = "insert into student (name, id) values (".strcat($scope.queryData.name, ",", $scope.queryData.name_id, ");");
+    var insertStudent = 'insert into student (name, id) values ('.concat($scope.queryData.name, ",", $scope.queryData.name_id, ");");
   
     $scope.query = function(data, queryStr){
       console.log(insertStudent);
@@ -34,9 +35,5 @@ angular.module('evvemiApp')
       });
     };
   
-    $scope.queryData = {
-      name_id: null,
-      name: ''
-    };
 
 });
